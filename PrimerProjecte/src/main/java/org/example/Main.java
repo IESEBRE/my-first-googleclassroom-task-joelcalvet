626db68d2a09a2aa1ef60e633e5b716024ac0098
«joelcalvet» <joelcalvet@iesebre.com>
2023-09-19 13:15:08 +0200

    Primer projecte

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //vaig a mostrar per la pantalla el missatge
+        System.out.println("    /\\");
+        System.out.println("   /  \\");
+        System.out.println("  /    \\");
+        System.out.println(" /      \\");
+        System.out.println("/   /\\   \\");
+        System.out.println("   /  \\");
+        System.out.println("   \\  /");
+        System.out.println("    \\/");
+        System.out.print("Me dic Joel ");
+        System.out.println("i soc estudiant");
+
+
+
+
+    }
+}
